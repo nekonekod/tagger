08af44a3c7f1c7bfb4d0483793b77b09c2805cf3
duwenjun <duwenjun@meituan.com>
2017-08-02 20:02:46 +0800

    eslint init

--- app/main.js
+++ app/main.js
@@ -1,64 +1,50 @@
 const electron = require('electron')
-// Module to control application life.
 const app = electron.app
-// Module to create native browser window.
 // 创建窗口的模块
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 // 保持一个全局的引用，以防被自动回收
 let mainWindow
 
 function createWindow () {
-  // Create the browser window.
-  //创建窗口
+  // 创建窗口
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
   // 渲染 index.html界面
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'views/index.html'),
     protocol: 'file:',
-    slashes: true
+    slashes: true,
   }))
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // 打开开发人员工具
+  mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
+  // 关闭窗口时调用
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+    // 释放窗口的引用，如果是多窗口的引用，应该使用一个数组来维护引用
     mainWindow = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// 在electron初始化结束后悔调用，之后才能创建窗口以及使用一些API
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
+// 窗口都关闭后退出程序
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+  // 对于OS X用户，使用cmd+q后才退出应用
   if (process.platform !== 'darwin') {
-      app.quit()
+    app.quit()
   }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+  // 对于OS X用户，点击dock图标来重建窗口
   if (mainWindow === null) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
